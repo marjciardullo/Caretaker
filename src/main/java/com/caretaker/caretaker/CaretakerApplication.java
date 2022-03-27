5af29b44d9f7d569379a379de6a800ab9e7a7a4b
marjorye.ramirez <marjorye.ramirez@birdie.ai>
2022-03-27 12:20:37 -0300

    Feat: caretaker api

---
+++ src/main/java/com/caretaker/caretaker/CaretakerApplication.java
@@ -0,0 +1,13 @@
+package com.caretaker.caretaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaretakerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaretakerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/caretaker/caretaker/CaretakerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caretaker.caretaker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaretakerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
