4f19854009c670597ebe61ad020a3f3b6bbd69f1
Camille Gachido <camillegachido70@gmail.com>
2022-05-22 11:17:40 -0300

    --adding MedicamentoUsuario model

---
+++ src/main/java/com/caretaker/caretaker/model/MedicamentoUsuarioId.java
@@ -0,0 +1,9 @@
+package com.caretaker.caretaker.model;
+
+import java.io.Serializable;
+
+public class MedicamentoUsuarioId implements Serializable {
+	private Usuario usuario;
+    private Medicamento medicamento;
+    
+}
